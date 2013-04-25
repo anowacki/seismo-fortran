fa3bcdabbb458c6faa0f4dcec4855e27c8b5be6e
Andy Nowacki <andy.nowacki@bristol.ac.uk>
2013-04-25 11:40:49 +0100

    Fixed non-functional test_CIJ_tandon_and_weng.f90

--- anisotropy_ajn/test_CIJ_tandon_and_weng.f90
+++ anisotropy_ajn/test_CIJ_tandon_and_weng.f90
@@ -19,7 +19,7 @@ implicit none
 
       real(rs), parameter :: big_number = 10.e36 ;      
 
-   real(rs)   :: CC(6,6),vp,vs,rho,vpi,vsi,rhoi,c,del
+   real(rs)   :: CC(6,6),CCexp(6,6),vp,vs,rho,vpi,vsi,rhoi,c,del,rhoout
    
    vp = 5.8d3
    vs = 3.46d3
@@ -32,7 +32,7 @@ implicit none
    vsi = 0.d0
    rhoi = 2.2d3
 
-   call CIJ_tandon_and_weng(vp,vs,rho,del,c,vpi,vsi,rhoi,CC)
+   call CIJ_tandon_and_weng(vp,vs,rho,del,c,vpi,vsi,rhoi,CC,rhoout)
    
    write(*,'(a)')  'Output should look like: (x10^7)'
    write(*,'(a)') &
@@ -43,8 +43,22 @@ implicit none
 '    0.0000    0.0000    0.0000    0.0000    0.5574    0.0000',&
 '    0.0000    0.0000    0.0000    0.0000    0.0000    0.5574'
 
+   CCexp = 1.d7 * reshape( &
+   (/  3.2634,  0.9878,  0.9878,  0.0000,  0.0000,  0.0000, &
+       0.9878,  3.3358,  0.9784,  0.0000,  0.0000,  0.0000, &
+       0.9878,  0.9784,  3.3358,  0.0000,  0.0000,  0.0000, &
+       0.0000,  0.0000,  0.0000,  1.1787,  0.0000,  0.0000, &
+       0.0000,  0.0000,  0.0000,  0.0000,  0.5574,  0.0000, &
+       0.0000,  0.0000,  0.0000,  0.0000,  0.0000,  0.5574  /), (/6,6/))
+
    write(*,'(/a)') 'Output: (x10^7)'
    write(*,'(6f10.4)') CC/1.d7
    
+   ! Different if any value differs by more than 0.1 %
+   if (any(abs(CC - CCexp) > abs(CCexp/1.d4))) then
+      write(*,'(/a,e10.4)') 'TEST FAILED: total difference between sum of matrices = ',sum(abs(CC-CCexp))
+   else
+      write(*,'(/a)') 'Test passed'
+   endif
 
 end program test_CIJ_tandon_and_weng
