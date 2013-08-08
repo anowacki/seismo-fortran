d37fc4f7f675320f40774ddc36adb82fab1af814
Andy Nowacki <andy.nowacki@bristol.ac.uk>
2013-08-08 17:17:23 +0100

    Test for constant-dist with gcp_points
    Update test_gcp_points.f90 to also use the constant-spacing rule and plot
    this with plot_test_gcp_points.gmt so check that both  methods work.

--- test_spherical_geometry/test_gcp_points.f90
+++ test_spherical_geometry/test_gcp_points.f90
@@ -5,6 +5,7 @@ use spherical_geometry
 
 integer,parameter :: rs = 8, &
                      npts = 1000 ! Number of points along path
+integer :: npts_out
 real(rs) :: x1 =  0.,  y1 =  0.,  &
             x2 = 45.,  y2 = 45.  ! Endpoints
 real(rs) :: ds, lon(npts), lat(npts) ! Points on path
@@ -14,7 +15,16 @@ write(0,'(a,f0.1,1x,f0.1)') 'Start point : ',x1,y1,&
 
 call gcp_points(x1,y1,x2,y2,lon,lat,n=npts,degrees=.true.)
 
+write(*,'(a)') "> -W2p,red"
 do i=1,npts
+   write(*,*) lon(i),lat(i)
+enddo
+
+ds = 1.
+call gcp_points(x1,y1,x2,y2,lon,lat,ds=ds,npts=npts_out,degrees=.true.)
+
+write(*,'(a)') "> -W2p,green,."
+do i=1,npts_out
    write(*,*) lon(i),lat(i)
 enddo
 
