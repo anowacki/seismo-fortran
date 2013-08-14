573287676b28be178b2ec0edbb033078ca44ad6c
Andy Nowacki <andy.nowacki@bristol.ac.uk>
2013-08-14 09:35:50 +0100

    Add progs/azi to compute forward azimuth between two points on a sphere

---
+++ progs/src/azi.f90
@@ -0,0 +1,29 @@
+program azi_prog
+! Program to take two sets of spherical co-ordinates
+! and write out an azimuth from the first to the second.
+
+use spherical_geometry, only: azimuth
+
+implicit none
+
+integer, parameter :: rs = selected_real_kind(15,307)
+real(rs) :: lon1,lat1,lon2,lat2
+integer :: iostatus
+
+if (iargc() /= 0) then
+	write(0,'(a)') 'Usage: azi < [stlon stlat evlon evlat]'
+	stop
+endif
+
+iostatus = 0
+do while (iostatus == 0)
+	read(*,*,iostat=iostatus) lon1,lat1,lon2,lat2
+	if (iostatus < 0) exit
+	if (iostatus > 0) then
+		write(0,'(a)') 'azi: Error: problem reading coordinates from stdin.'
+		stop
+	endif
+	write(*,'(f10.6)') azimuth(lon1,lat1,lon2,lat2,degrees=.true.)
+enddo
+
+end program azi_prog
