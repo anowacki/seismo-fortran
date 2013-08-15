5093b2eb918fe70b9e8835f869db684e8529d259
Andy Nowacki <andy.nowacki@bristol.ac.uk>
2013-08-15 10:32:07 +0100

    Add progs/gcp_from_point{s,_azi}, for spherical_geometry routines
    Add two wrapper programs which provide command-line access to the
    functionality in the respective routines in spherical_geometry.

---
+++ progs/src/gcp_from_point_azi.f90
@@ -0,0 +1,42 @@
+!===============================================================================
+program gcp_from_point_azi_prog
+!===============================================================================
+! Wrapper program for sg_gcp_from_point_azi.  Given a point on a sphere and an
+! azimuth through that point, return the pole to the great circle along that
+! azimuth.  Reads lon, lat and azi from stdin, all in degrees
+
+   use spherical_geometry, only: sg_gcp_from_point_azi
+
+   implicit none
+   
+   real(8) :: lon,lat,azi,long,latg
+   integer :: iostat
+
+   if (command_argument_count() /= 0) call usage
+   
+   iostat = 0
+   do while (iostat == 0)
+      read(*,*,iostat=iostat) lon,lat,azi
+      if (iostat > 0) then
+         write(0,'(a)') 'gcp_from_point_azi: Error: problem reading lon,lat,azi (degrees) from stdin'
+         stop
+      endif
+      if (iostat < 0) exit
+      call sg_gcp_from_point_azi(lon,lat,azi,long,latg,degrees=.true.)
+      write(*,'(f11.6,1x,f10.6)') long,latg
+   enddo
+
+contains
+   !============================================================================
+   subroutine usage()
+   !============================================================================
+      implicit none
+      write(0,'(a)') 'Usage: gcp_from_point_azi < [lon] [lat] [azi]',&
+                     '   Reads point on sphere and azimuth from stdin (degrees)',&
+                     '   Writes lon and lat of pole to great circle defined by the input to stdout.'
+      stop
+   end subroutine usage
+   !----------------------------------------------------------------------------
+
+end program gcp_from_point_azi_prog
+!-------------------------------------------------------------------------------
---
+++ progs/src/gcp_from_points.f90
@@ -0,0 +1,42 @@
+!===============================================================================
+program gcp_from_points_prog
+!===============================================================================
+! Wrapper program for sg_gcp_from_s.  Given a point on a sphere and an
+! azimuth through that point, return the pole to the great circle along that
+! azimuth.  Reads lon, lat and azi from stdin, all in degrees
+
+   use spherical_geometry, only: sg_gcp_from_points
+
+   implicit none
+
+   real(8) :: lon1,lat1,lon2,lat2,long,latg
+   integer :: iostat
+
+   if (command_argument_count() /= 0) call usage
+
+   iostat = 0
+   do while (iostat == 0)
+      read(*,*,iostat=iostat) lon1,lat1,lon2,lat2
+      if (iostat > 0) then
+         write(0,'(a)') 'gcp_from_points: Error: problem reading lon1,lat1,lon2,lat2 (degrees) from stdin'
+         stop
+      endif
+      if (iostat < 0) exit
+      call sg_gcp_from_points(lon1,lat1,lon2,lat2,long,latg,degrees=.true.)
+      write(*,'(f11.6,1x,f10.6)') long,latg
+   enddo
+
+contains
+   !============================================================================
+   subroutine usage()
+   !============================================================================
+      implicit none
+      write(*,'(a)') 'Usage: gcp_from_points < [lon1] [lat1] [lon2] [lat2]',&
+                     '   Reads two point on sphere from stdin (degrees)',&
+                     '   Writes lon and lat of pole to great circle defined by the input to stdout.'
+      stop
+   end subroutine usage
+   !----------------------------------------------------------------------------
+
+end program gcp_from_points_prog
+!-------------------------------------------------------------------------------
