aed57ec961924126650bedcc1ebd4f58a401df1a
Andy Nowacki <andy.nowacki@bristol.ac.uk>
2015-02-10 12:16:47 +0000

    sph_tess: Update program to use caching
    sph_tess now automatically loads tesselations from the global
    cache directory when they exist, and saves them when they don't.
    There is currently no way to prevent this happening other than
    setting the $SEISMO_FORTRAN_DATA environment variable
    to a directory where the cache files do not exist or moving the
    cache files.  They will still be generated.

--- progs/src/sph_tess.f90
+++ progs/src/sph_tess.f90
@@ -1,8 +1,17 @@
 !===============================================================================
+! One of Andy Nowacki's Fortran utility modules for dealing with seismic
+! anisotropy and other problems.
+!
+! Andy Nowacki <andy.nowacki@bristol.ac.uk>
+!
+! See the file LICENCE for licence details.
+!===============================================================================
 program sph_tess
 !===============================================================================
 ! Dump the points for a level-n tesselation of a sphere, starting with an
 ! icosahedron.
+! This version will create a tesselation of higher order if it is not available
+! as a pre-saved version on disk.
 
    use sphere_tesselate
    use spherical_geometry
@@ -10,14 +19,25 @@ program sph_tess
    implicit none
    type(tesselation) :: t
    integer :: i, n
-   logical :: output_triangles = .false., output_geog = .false.
+   logical :: output_triangles = .false., output_geog = .false., &
+      cache_exists = .false.
+   ! Path to directory containing pre-computed tesselations
+   character(len=250) :: file
 
    call get_args
 
-   t = st_icosahedron(pole=.true.)
-   do i = 1, n
-      call st_iterate_level(t)
-   enddo
+   ! Try to load the file from the global cache
+   call st_load_cache(t, cache_exists)
+   ! Otherwise, create the tesselation and save the cache file for later use
+   if (.not.cache_exists) then
+      write(0,'(a)') 'Generating tesselation and saving cache file...'
+      t = st_icosahedron(pole=.true.)
+      do i = 1, n
+         call st_iterate_level(t)
+      enddo
+      call st_save_cache(t)
+   endif
+   ! Output
    if (.not.output_triangles) then
       call st_dump_points(t, geog=output_geog)
    else
@@ -33,8 +53,9 @@ contains
          'icosahedron.  x, y, z values on the unit sphere are output to stdout.', &
          'n must be >= 0', &
          'Options:', &
-         '   -g : Write values as lon, lat instead of x, y, z', &
-         '   -t : Dump the triangles as multisegment lines'
+         '   -g        : Write values as lon, lat instead of x, y, z', &
+         '   -p [path] : Set directory path to precomputed tesselations', &
+         '   -t        : Dump the triangles as multisegment lines'
       stop
    end subroutine usage
 
@@ -48,14 +69,17 @@ contains
       do while (iarg <= narg - 1)
          call get_command_argument(iarg, arg)
          select case(arg)
-            case('-g');     output_geog = .true.;      iarg = iarg + 1
-            case('-t');     output_triangles = .true.; iarg = iarg + 1
-            case default;   call usage
+            case('-g');   output_geog = .true.;       iarg = iarg + 1
+            case('-p');   call get_command_argument(iarg+1, st_cache_dir)
+                                                      iarg = iarg + 2
+            case('-t');   output_triangles = .true.;  iarg = iarg + 1
+            case default; call usage
          end select
       enddo
       ! Get tesselation level
       call get_command_argument(narg, arg)
       read(arg,*) n
+      t%level = n
       if (n < 0) call usage
    end subroutine get_args
 
