26680b04dfde9823e481223296f8f444f01d833b
Andy Nowacki <a.nowacki@leeds.ac.uk>
2015-12-15 12:49:59 +0000

    progs/delta: Modernise source and add usage

--- progs/src/delta.f90
+++ progs/src/delta.f90
@@ -15,38 +15,51 @@ implicit none
 
 ! variables
 double precision :: lat1,lon1,lat2,lon2
-double precision :: angle,pi
-integer          :: iostatus
+double precision :: angle
+integer          :: iostatus = 0
 
-! write (*,*)"longitude1, latitude1, longitude2, latitude2"
-iostatus = 0
+call get_args
+
 do while (iostatus == 0)
-	read(*,*,iostat=iostatus) lon1,lat1,lon2,lat2
-	if (iostatus < 0) exit
-	if (iostatus > 0) stop 'Some problem reading coordinates'
-	write(*,'(f10.6," degrees")')angle(lat1,lon1,lat2,lon2)
+   read(*,*,iostat=iostatus) lon1,lat1,lon2,lat2
+   if (iostatus < 0) exit
+   if (iostatus > 0) stop 'Some problem reading coordinates'
+   write(*,'(f10.6)') angle(lat1,lon1,lat2,lon2)
 enddo
-end program
+
+contains
+
+subroutine get_args()
+   integer :: narg, iarg
+   narg = command_argument_count()
+   if (narg > 0) call usage
+end subroutine get_args
+
+subroutine usage()
+   write(0,'(a)') 'Usage: delta < [(lon1, lat1, lon2, lat2) on stdin]', &
+                  'Reads pairs of geographic coordinates in degrees and writes', &
+                  'great circle distance on a sphere (in degrees) to stdout.'
+   stop
+end subroutine usage
+
+end program delta
 
 
-
-
-
-function angle(lat1,lon1,lat2,lon2)
+function angle(lat1i,lon1i,lat2i,lon2i)
 implicit none
-double precision :: angle,lat1,lon1,lat2,lon2,pi
+double precision :: angle
+double precision, intent(in) :: lat1i,lon1i,lat2i,lon2i
+double precision :: lat1,lon1,lat2,lon2
+double precision, parameter :: pi=3.14159265358979323846264338327950288D0
 
-pi=3.14159265358979323846264338327950288D0
-
-lat1=lat1*pi/1.8D2 ; lon1=lon1*pi/1.8D2
-lat2=lat2*pi/1.8D2 ; lon2=lon2*pi/1.8D2
+lat1=lat1i*pi/1.8D2 ; lon1=lon1i*pi/1.8D2
+lat2=lat2i*pi/1.8D2 ; lon2=lon2i*pi/1.8D2
 
 angle=atan2( sqrt( (cos(lat2)*sin(lon2-lon1))**2 + (cos(lat1)*sin(lat2) - &
-		sin(lat1)*cos(lat2)*cos(lon2-lon1))**2) , &
-		sin(lat1)*sin(lat2) + cos(lat1)*cos(lat2)*cos(lon2-lon1))
+                   sin(lat1)*cos(lat2)*cos(lon2-lon1))**2) , &
+            sin(lat1)*sin(lat2) + cos(lat1)*cos(lat2)*cos(lon2-lon1))
 
 angle=angle*1.8D2/pi
 
-return
-end function
-!=ACOS(SIN(lat1)*SIN(lat2)+COS(lat1)*COS(lat2)*COS(lon2-lon1))*6371
+end function angle
+
